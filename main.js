924f5c9a32410896b345641c25f43b30c0d4d173
GtGnzz <111307507+GtGnzz@users.noreply.github.com>
2023-10-09 04:24:07 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const menu = document.querySelector('.menu');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+
